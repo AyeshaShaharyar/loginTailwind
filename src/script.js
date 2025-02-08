bbdc2f6f0b9d394934c8bfaef3bc5c45ff733bea
AyeshaShaharyar <ayesha2598@gmail.com>
2025-02-08 17:17:58 -0500

    add functionality

--- src/script.js
+++ src/script.js
@@ -9,6 +9,32 @@ const users = [
   { username: "user1", password: "123abc" },
 ];
 
-function login(){
-  
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  //check if user exists in db
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+
+  if (user) {
+    //login
+    document.getElementById("login-form").classList.add("hidden");
+    document.getElementById("dashboard").classList.remove("hidden");
+    document.getElementById("user-name").innerText = username;
+  } else {
+    //show error message
+    message.innerText = "Invalid username or password.";
+  }
+}
+
+function logout() {
+  //reset the states
+  document.getElementById("dashboard").classList.add("hidden");
+  document.getElementById("login-form").classList.remove("hidden");
+  document.getElementById("message").innerText = "";
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      colors: {
-        lightBlue: "#D1F8EF",
-        skyBlue: "#A1E3F9"
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
